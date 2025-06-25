cbb6a1272b92f35517e50e499415500e565ae1ab
Sanskar <jaiswalsanskar.2020@gmail.com>
2025-06-25 14:24:28 +0530

    Created Todo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import express from "express";
-
 const app = express();
-
-
+import todoRoutes from "./routes/todoRoutes.js"
 
 app.use(express.json());
 
+app.use('/api/v1/todos', todoRoutes)
 
 export default app;
---
+++ src/controllers/todoController.js
@@ -0,0 +1,19 @@
+import { Todo } from "../models/todoModels.js";
+
+//create a Todo
+export const createTodo = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const newTodo = await Todo.create({
+      title,
+      description,
+    });
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+//read a todo
+//update
+//delete
