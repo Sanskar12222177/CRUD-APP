4724bf270eee6c019034ae36847499f09264a2d5
Sanskar <jaiswalsanskar.2020@gmail.com>
2025-06-25 13:16:03 +0530

    Users details

---
+++ src/models/users.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const usersSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    username : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+
+})
+
+export const user = mongoose.model('Users', usersSchema);
