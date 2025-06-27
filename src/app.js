fa8610b25d00fa5602441561d5bbae75d8894a0a
Sanskar <jaiswalsanskar.2020@gmail.com>
2025-06-27 11:27:05 +0530

    perform CRUD operation

--- src/app.js
+++ src/app.js
@@ -6,5 +6,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use('/api/v1/todos', todoRoutes);
+//http://localhost:8000/api/v1/todos
 
 export default app;
--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -4,10 +4,7 @@ import { Todo } from "../models/todoModels.js";
 export const createTodo = async (req, res) => {
   try {
     const { title, description } = req.body;
-    const newTodo = await Todo.create({
-      title,
-      description,
-    });
+    const newTodo = await Todo.create({ title,description });
     res.status(201).json(newTodo);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -21,10 +18,42 @@ export const createTodo = async (req, res) => {
 //create
 // export const createTodo = () => {}
 //read all
-export const getTodo = () => {}
+export const getTodo = async(req,res) => {
+  try{
+    const todos = await Todo.find();
+  res.status(201).json(todos)
+  } catch(error){
+    res.status(404).json({ message: error.message });
+  }
+}
+
+
 //read one
-export const getTodoByID = () => {}
-//delete
-export const deleteTodo = () => {}
+export const getTodoByID = async (req,res) => {
+  try {
+  const todo = await Todo.findById(req.params.id);
+  res.status(201).json(todo)
+  } catch (error){
+    res.status(404).json({ message: error.message });
+  }
+}
 //update
-export const updateTodo = () => {}
+export const updateTodo = async(req,res) => {
+    try {
+        const updateTodo = await Todo.findByIdAndUpdate(req.params.id, req.body,{new: true});
+        res.json(updateTodo)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+
+//delete
+export const deleteTodo = async() => {
+    try {
+        const deleteTodo = await Todo.findByIdAndDelete(req.params.id);
+        res.json({message : "Todo Deleted"})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
